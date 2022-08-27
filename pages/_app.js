3e724470c44aa8ec6ae14d05dcc5155e49b6c51e
root <root@Laptop.localdomain>
2022-08-27 22:20:48 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+import { NotificationProvider } from "web3uikit"
+
+let APP_ID = process.env.NEXT_PUBLIC_APP_ID
+let SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </div>
+    )
+}
+
+export default MyApp
